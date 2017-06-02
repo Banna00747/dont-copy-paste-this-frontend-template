19ea1ae547a827c43e9124cae947f4a38da3f812
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-06-02 14:49:35 +0300

    Implement

--- scripts/config.js
+++ scripts/config.js
@@ -4,16 +4,18 @@ const _ = require('lodash');
 // envify is a Browserify transform which replaces all process.env.X references
 // with build time environment variables.
 // You must e.g. do
-//    API_URL=x npm run build
+//    API_HOST=x npm run build
 //
 // to change this variable.
-if (_.isEmpty(process.env.API_URL)) {
-  let message = 'API_URL is not defined. You must define API_URL environment'
+if (_.isEmpty(process.env.API_HOST)) {
+  let message = 'API_HOST is not defined. You must define API_HOST environment'
   message += ' variable when building the frontend.'
 
   throw new Error(message);
 }
 
 module.exports = {
-  API_URL: process.env.API_URL
+  API_HOST: process.env.API_HOST,
+  API_HTTP_URL: `http://${process.env.API_HOST}`,
+  API_WS_URL: `ws://${process.env.API_HOST}`,
 };
